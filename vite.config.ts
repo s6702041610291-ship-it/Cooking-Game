0b30363738961d898a6d82e8036d71a6b30cb774
s6702041610291-ship-it <s6702041610291@email.kmutnb.ac.th>
2026-09-19 22:13:36 +0700

    chore: refactor project configuration and metadata
    Update project name and version, clean up unused dependencies, adjust path aliasing for Vite, and add Vercel deployment configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig} from 'vite';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
     server: {
